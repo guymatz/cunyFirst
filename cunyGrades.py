6430942de344125f111276e37cd0a452e90fd8a8
Guy Matz <gmatz@matz.org>
2016-12-25 11:13:30 -0500

    working.  change passqword

--- cunyGrades.py
+++ cunyGrades.py
@@ -1,20 +1,20 @@
 import sys
 from mechanize import Browser, Request
 import urllib
-import Cookie
 import cookielib
 import argparse
-#from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
+import xml.etree.ElementTree
 from datetime import datetime
 
-url='https://hrsa.cunyfirst.cuny.edu/oam/Portal_Login1.html'
+url = 'https://hrsa.cunyfirst.cuny.edu/oam/Portal_Login1.html'
 jar = cookielib.LWPCookieJar()
 br = Browser()
 br.set_cookiejar(jar)
 br.set_handle_robots(False)
 br.open(url)
 username = 'Guy.Matz77'
-password = '*********'
+password = CHANGEME
 br.select_form(nr=0)
 br["login"] = username
 br["password"] = password
@@ -26,66 +26,63 @@ def w(ofile, browser):
         f.write(browser.response().get_data())
 
 
-br.set_debug_http(sys.stdout)
-br.set_debug_redirects(sys.stdout)
-br.set_debug_responses(sys.stdout)
-        
-br.open("https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE&Action=A&TargetFrameName=None")
+# br.set_debug_http(sys.stdout)
+# br.set_debug_redirects(sys.stdout)
+# br.set_debug_responses(sys.stdout)
+
+#br.open("https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE&Action=A&TargetFrameName=None")
+br.open("https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL")
 w('now', br)
 
 br.select_form(nr=0)
-ICSID = br.form['ICSID']
+f = br.form
+change_term_url = f.action
+data = {}
+for k,v in f.click_pairs():
+    data[k] = v
 
-change_term_url = 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL'
-
-data={
-    'ICAJAX': '1',
-    'ICNAVTYPEDROPDOWN': '0',
-    'ICType': 'Panel',
-    'ICElementNum': '0',
-    'ICStateNum': '2',
-    'ICAction': 'DERIVED_SSS_SCT_SSS_TERM_LINK',
-    'ICXPos': '0',
-    'ICYPos': '0',
-    'ResponsetoDiffFrame': '-1',
-    'TargetFrameName': 'None',
-    'FacetPath': 'None',
-    'ICFocus': '',
-    'ICSaveWarningFilter': '0',
-    'ICChanged': '-1',
-    'ICAutoSave': '0',
-    'ICResubmit': '0',
-    'ICActionPrompt': 'false',
-    'ICBcDomData': 'undefined',
-    'ICFind': '',
-    'ICAddCount': '',
-    'ICAPPCLSDATA': '',
-    'DERIVED_SSTSNAV_SSTS_MAIN_GOTO$7$': '9999',
-    'DERIVED_SSTSNAV_SSTS_MAIN_GOTO$8$': '9999',
-    'ptus_defaultlocalnode': 'PSFT_CNYHCPRD',
-    'ptus_dbname': 'CNYHCPRD',
-    'ptus_portal': 'EMPLOYEE',
-    'ptus_node': 'HRMS',
-    'ptus_workcenterid': '',
-    'ptus_componenturl': 'https://hrsa.cunyfirst.cuny.edu/psp/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL'
-    }
-data['ICSID'] = ICSID
-data['ICAction']='DERIVED_SSS_SCT_SSR_PB_GO'
-data['SSR_DUMMY_RECV1$sels$1$$0']='1'
-
+data['ICAction'] = 'DERIVED_SSS_SCT_SSS_TERM_LINK'
+# Do I need this section?
 encData = urllib.urlencode(data)
 req = Request(change_term_url, data=encData, headers={"Content-type": "application/x-www-form-urlencoded"})
 br.open(req)
 w('now1', br)
 
-##@# data['ICAction']='DERIVED_SSS_SCT_SSR_PB_GO'
-##@# data['SSR_DUMMY_RECV1$sels$1$$0']='1'
-##@# 
-##@# encData = urllib.urlencode(data)
-##@# req = Request(change_term_url, data=encData, headers={"Content-type": "application/x-www-form-urlencoded"})
-##@# br.open(req)
-##@# w('now2', br)
-##@# 
-##@# 
-##@# br.open('https://home.cunyfirst.cuny.edu/psp/cnyepprd/EMPLOYEE/EMPL/?cmd=logout')
-##@# 
+br.select_form(nr=0)
+f = br.form
+change_term_url = f.action
+data = {}
+for k,v in f.click_pairs():
+    #print("Adding %s : %s" % (k,v))
+    data[k] = v
+
+data['ICAction'] = 'DERIVED_SSS_SCT_SSR_PB_GO'
+data['ICAJAX'] = '1'
+data['ICNAVTYPEDROPDOWN'] = '0'
+data['ResponsetoDiffFrame'] = '-1'
+data['SSR_DUMMY_RECV1$sels$1$$0'] = '1'
+data['ICBcDomData'] = 'undefined'
+data['SSR_DUMMY_RECV1$sels$0'] = '1'
+##@#
+encData = urllib.urlencode(data)
+req = Request(change_term_url, data=encData, headers={"Content-type": "application/x-www-form-urlencoded"})
+# br.set_debug_http(sys.stdout)
+#br.set_debug_redirects(sys.stdout)
+#br.set_debug_responses(sys.stdout)
+br.open(req)
+
+new_text = xml.etree.ElementTree.fromstring(br.response().get_data())[6].text
+
+try:
+    old_html = xml.etree.ElementTree.parse('now2.html').getroot()[6].text
+    if old_html != new_html:
+        print("Something Changed!")
+        w('now3', br)
+    else:
+        w('now2', br)
+except Exception as e:
+    w('now2', br)
+
+br.open('https://home.cunyfirst.cuny.edu/psp/cnyepprd/EMPLOYEE/EMPL/?cmd=logout')
+
+
